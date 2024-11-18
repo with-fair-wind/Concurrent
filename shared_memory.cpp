d392ec2e83373b86fc0f97a17d1cde4d11819516
kk <yangyukai@zwcad.com>
2024-11-18 17:45:09 +0800

    contiue shared_memory -> v_3

--- shared_memory.cpp
+++ shared_memory.cpp
@@ -111,16 +111,15 @@ void thread_function(int id)
     // 尝试加锁
     if (mtx.try_lock())
     {
-        std::string s = "线程：" + std::to_string(id) + " 获得锁";
-        std::cout << s << std::endl;
+        std::osyncstream{std::cout} << "线程：" << id << " 获得锁" << std::endl;
         // 临界区代码
         std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 模拟临界区操作
         mtx.unlock();                                                // 解锁
-        std::cout << "线程：" << id << " 释放锁" << std::endl;
+        std::osyncstream{std::cout} << "线程：" << id << " 释放锁" << std::endl;
     }
     else
     {
-        std::cout << "线程：" << id << " 获取锁失败 处理步骤" << std::endl;
+        std::osyncstream{std::cout} << "线程：" << id << " 获取锁失败 处理步骤" << std::endl;
     }
 }
 
