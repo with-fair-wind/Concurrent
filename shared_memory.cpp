cbdb5c138c4054e6c54c25f3d1ec2c5162c8ee62
kk <yangyukai@zwcad.com>
2024-11-22 17:32:04 +0800

    contiue shared_memory -> v_5

--- shared_memory.cpp
+++ shared_memory.cpp
@@ -12,7 +12,7 @@
 #include <vector>
 using namespace std::chrono_literals;
 
-#define VERSION_3
+#define VERSION_5
 
 #ifdef VERSION_1
 // 条件竞争
@@ -132,4 +132,70 @@ int main()
     t2.join();
 }
 
+#elif defined(VERSION_4)
+// 切勿将受保护数据的指针或引用传递到互斥量作用域之外，不然保护将形同虚设
+class Data
+{
+    int a{};
+    std::string b{};
+
+public:
+    void do_something()
+    {
+        // 修改数据成员等...
+    }
+};
+
+class Data_wrapper
+{
+    Data data;
+    std::mutex m;
+
+public:
+    template <class Func>
+    void process_data(Func func)
+    {
+        std::lock_guard<std::mutex> lc{m};
+        func(data); // 受保护数据传递给函数
+    }
+};
+
+Data *p = nullptr;
+
+void malicious_function(Data &protected_data)
+{
+    p = &protected_data; // 受保护的数据被传递到外部
+}
+
+Data_wrapper d;
+
+void foo()
+{
+    d.process_data(malicious_function); // 传递了一个恶意的函数
+    p->do_something();                  // 在无保护的情况下访问保护数据
+}
+#elif defined(VERSION_5)
+// 死锁
+std::mutex m1, m2;
+std::size_t n{};
+void f1()
+{
+    std::lock_guard<std::mutex> lc1{m1};
+    std::this_thread::sleep_for(5ms);
+    std::lock_guard<std::mutex> lc2{m2};
+    ++n;
+}
+void f2()
+{
+    std::lock_guard<std::mutex> lc1{m2};
+    std::this_thread::sleep_for(5ms);
+    std::lock_guard<std::mutex> lc2{m1};
+    ++n;
+}
+
+int main()
+{
+    std::jthread t1{f1};
+    std::jthread t2{f2};
+}
 #endif
