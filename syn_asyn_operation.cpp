80b6fbd058684d41a22d7de854ed2c4e583c60b2
kk <921232958@qq.com>
2024-12-06 19:18:42 +0800

    contiue syn_asyn_operation -> v_10

--- syn_asyn_operation.cpp
+++ syn_asyn_operation.cpp
@@ -25,7 +25,7 @@ using namespace std::chrono_literals;
 在多线程编程中，各个任务通常需要通过**同步设施**进行相互**协调和等待**，以确保数据的**一致性**和**正确性**
 */
 
-#define VERSION_6
+#define VERSION_11
 #ifdef VERSION_1
 /*
 等待事件及条件
@@ -553,7 +553,7 @@ void throw_function(std::promise<int> prom)
     {
         try
         {
-            // 共享状态的 promise 已存储值，调用 set_exception 产生异常
+            // 共享状态的 promise 已存储值，调用 set_exception/set_value 产生异常
             prom.set_exception(std::current_exception());
             // prom.set_value(50);
         }
@@ -578,4 +578,234 @@ int main()
 }
 #endif
 
+#elif defined(VERSION_7)
+// future 是一次性的，需要注意移动。且调用 get 函数后，future 对象也会失去共享状态
+/*
+1:移动语义：移动操作标志着所有权的转移，意味着 future 不再拥有共享状态（如之前所提到）。get 和 wait 函数要求 future 对象拥有共享状态，否则会抛出异常。
+2:共享状态失效：调用 get 成员函数时，future 对象必须拥有共享状态，但调用完成后，它就会失去共享状态，不能再次调用 get
+*/
+
+// 在 get 函数中: future _Local{_STD move(*this)}; 将当前对象的共享状态转移给了这个局部对象，而局部对象在函数结束时析构。这意味着当前对象失去共享状态，并且状态被完全销毁。
+int main()
+{
+    std::future<void> future = std::async([] {});
+    std::cout << std::boolalpha << future.valid() << '\n'; // true
+    future.get();
+    std::cout << std::boolalpha << future.valid() << '\n'; // false
+    try
+    {
+        future.get(); // 抛出 future_errc::no_state 异常
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+#elif defined(VERSION_8)
+// 多个线程的等待 std::shared_future
+// std::future 有一个局限：future 是一次性的，它的结果只能被一个线程获取。get() 成员函数只能调用一次，当结果被某个线程获取后，std::future 就无法再用于其他线程
+
+#if 0
+// 可能有多个线程都需要耗时的异步任务的返回值
+int task()
+{
+    // todo...
+    return 10;
+}
+
+void thread_functio1(std::shared_future<int> fut)
+// void thread_functio1(std::shared_future<int> &fut)
+// void thread_functio1(std::future<int> &fut)
+{
+    // todo...
+    try
+    {
+        int res = fut.get();
+        std::cout << "func1 need: " << res << "\n";
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    // todo...
+}
+
+void thread_functio2(std::shared_future<int> fut)
+// void thread_functio2(std::shared_future<int> &fut)
+// void thread_functio2(std::future<int> &fut)
+{
+    // todo...
+    try
+    {
+        int res = fut.get();
+        std::cout << "func2 need: " << res << "\n";
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    // todo...
+}
+
+int main()
+{
+    std::shared_future<int> future = std::async(task);
+    std::jthread t1{thread_functio1, future};
+    std::jthread t2{thread_functio2, future};
+    // std::jthread t1{thread_functio1, std::ref(future)};
+    // std::jthread t2{thread_functio2, std::ref(future)};
+}
+#else
+// 此时就需要使用 std::shared_future 来替代 std::future 了。
+// std::future 与 std::shared_future 的区别就如同 std::unique_ptr、std::shared_ptr 一样
+// std::future 是只能移动的，其所有权可以在不同的对象中互相传递，但只有一个对象可以获得特定的同步结果
+// 而 std::shared_future 是可复制的，多个对象可以指代同一个共享状态
+
+/*
+***
+在多个线程中对"同一个" std::shared_future 对象进行操作时（如果没有进行同步保护）存在条件竞争。
+而从多个线程访问同一共享状态，若每个线程都是通过其自身的 shared_future 对象副本进行访问，则是安全的
+***
+*/
+std::string fetch_data()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // 模拟耗时操作
+    return "从网络获取的数据！";
+}
+
+int main()
+{
+    std::future<std::string> future_data = std::async(std::launch::async, fetch_data);
+
+    // // 转移共享状态，原来的 future 被清空  valid() == false
+    std::shared_future<std::string> shared_future_data = future_data.share();
+#if 0
+    // 多个线程持有一个 shared_future对象并操作
+    // 这段代码存在数据竞争, 它并没有提供线程安全的方式, lambda 是按引用传递，也就是“同一个”进行操作了。
+
+    // 第一个线程等待结果并访问数据
+    std::thread thread1([&shared_future_data]
+                        {
+        std::cout << "线程1:等待数据中..." << std::endl;
+        shared_future_data.wait();
+        std::cout << "线程1:收到数据:" << shared_future_data.get() << std::endl; });
+
+    // 第二个线程等待结果并访问数据
+    std::thread thread2([&shared_future_data] // 多个线程持有一个 shared_future对象并操作
+                        {
+        std::cout << "线程2:等待数据中..." << std::endl;
+        shared_future_data.wait();
+        std::cout << "线程2:收到数据:" << shared_future_data.get() << std::endl; });
+#else
+    // 第一个线程等待结果并访问数据
+    std::thread thread1([shared_future_data]
+                        {
+        std::cout << "线程1:等待数据中..." << std::endl;
+        shared_future_data.wait();
+        std::cout << "线程1:收到数据:" << shared_future_data.get() << std::endl; });
+
+    // 第二个线程等待结果并访问数据
+    std::thread thread2([shared_future_data] // 多个线程持有一个 shared_future对象并操作
+                        {
+        std::cout << "线程2:等待数据中..." << std::endl;
+        shared_future_data.wait();
+        std::cout << "线程2:收到数据:" << shared_future_data.get() << std::endl; });
+#endif
+    thread1.join();
+    thread2.join();
+}
+#endif
+
+#elif defined(VERSION_9)
+// 返回类型是一个枚举类 std::future_status ，三个枚举项分别表示三种 future 状态。
+/*
+deferred 共享状态持有的函数正在延迟运行，结果将仅在明确请求时计算
+ready 共享状态就绪
+timeout 共享状态在经过指定的等待时间内仍未就绪
+*/
+
+int main()
+{
+#define TimeOut
+#ifdef Deferred
+    auto future = std::async(std::launch::deferred, []
+                             { std::cout << "deferred\n"; });
+    if (future.wait_for(35ms) == std::future_status::deferred)
+        std::cout << "std::launch::deferred" << '\n';
+    future.wait(); // 在 wait() 或 get() 调用时执行，不创建线程
+#elif defined(Ready)
+    std::future<int> future = std::async([]
+                                         { 
+                                            std::this_thread::sleep_for(34ms);
+                                            return 6; });
+    if (future.wait_for(35ms) == std::future_status::ready)
+        std::cout << future.get() << '\n';
+    else
+        std::cout << "not ready\n";
+#elif defined(TimeOut)
+    std::future<int> future = std::async([]
+                                         { 
+                                            std::this_thread::sleep_for(36ms);
+                                            return 6; });
+    if (future.wait_for(35ms) == std::future_status::timeout)
+        std::cout << "任务还未执行完毕\n";
+    else
+        std::cout << "任务执行完毕\n"
+                  << future.get() << std::endl;
+#endif
+}
+#elif defined(VERSION_10)
+// Windows 内核中的时间间隔计时器默认每隔 15.6 毫秒触发一次中断。因此，如果你使用基于系统时钟的计时方法，默认情况下精度约为 15.6 毫秒。不可能达到纳秒级别。
+#ifdef _MSC_VER
+#pragma comment(lib, "mylib.lib")
+#endif
+#include "Windows.h"
+int main()
+{
+    // timeBeginPeriod(1);
+    auto start = std::chrono::system_clock::now();
+    std::this_thread::sleep_for(1s);
+    auto end = std::chrono::system_clock::now();
+    auto res = std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, 1000>>>(end - start);
+    std::cout << res.count() << std::endl;
+    // timeEndPeriod(1);
+}
+#elif defined(VERSION_11)
+std::condition_variable cv;
+bool done{};
+std::mutex m;
+
+bool wait_loop()
+{
+    const auto timeout = std::chrono::steady_clock::now() + 500ms;
+    std::unique_lock<std::mutex> lk{m};
+#if 0
+    while (!done)
+    {
+        if (cv.wait_until(lk, timeout) == std::cv_status::timeout)
+        {
+            std::cout << "超时 500ms\n";
+            return false;
+        }
+    }
+#else
+    if (!cv.wait_until(lk, timeout, []
+                       { return done; }))
+    {
+        std::cout << "超时 500ms\n";
+        return false;
+    }
+#endif
+    return true;
+}
+
+int main()
+{
+    std::thread t{wait_loop};
+    std::this_thread::sleep_for(600ms);
+    done = true;
+    cv.notify_one();
+    t.join();
+}
 #endif
