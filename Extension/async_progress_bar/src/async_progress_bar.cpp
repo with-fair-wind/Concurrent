4ff7f53c1c1f20fe4bdf1ca3377b496f48f95047
kk <921232958@qq.com>
2025-06-27 15:39:06 +0800

    update

--- Extension/async_progress_bar/src/async_progress_bar.cpp
+++ Extension/async_progress_bar/src/async_progress_bar.cpp
@@ -1,4 +1,5 @@
 #include "async_progress_bar.h"
+#include <windows.h>
 
 async_progress_bar::async_progress_bar(QWidget *parent) : QWidget{parent},
                                                           progress_bar{new QProgressBar(this)},
@@ -40,7 +41,8 @@ void async_progress_bar::task()
                         {
             QMetaObject::invokeMethod(this, [this] {
                 // 这里显示的线程 ID 就是主线程，代表这些任务就是在主线程，即 UI 线程执行
-                QMessageBox::information(nullptr, "线程ID", std::to_string(pthread_self()).c_str());
+                // QMessageBox::information(nullptr, "线程ID", std::to_string(pthread_self()).c_str());
+                QMessageBox::information(nullptr, "线程ID", std::to_string(GetCurrentThreadId()).c_str());
                 button->setEnabled(false);
                 progress_bar->setRange(0, 1000);
                 button->setText("正在执行...");
@@ -56,7 +58,8 @@ void async_progress_bar::task()
                 button->setEnabled(true);
             });
             // 不在 invokeMethod 中获取线程 ID，这里显示的是子线程的ID
-            auto s = std::to_string(pthread_self());
+            // auto s = std::to_string(pthread_self());
+            auto s = std::to_string(GetCurrentThreadId());
             QMetaObject::invokeMethod(this, [=] {
                 QMessageBox::information(nullptr, "线程ID", s.c_str());
             }); });
--- Extension/async_progress_bar/src/main.cpp
+++ Extension/async_progress_bar/src/main.cpp
@@ -1,13 +1,15 @@
 #include "async_progress_bar.h"
 
 #include <QApplication>
+#include <windows.h>
 #pragma comment(lib, "user32.lib")
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    auto s = std::to_string(pthread_self());
+    // auto s = std::to_string(pthread_self());
+    auto s = std::to_string(GetCurrentThreadId());
     QMessageBox::information(nullptr, "主线程ID", s.c_str());
 
     async_progress_bar w;
